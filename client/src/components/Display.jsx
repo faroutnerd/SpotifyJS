ce32f2e8140709c11719f26dad4625d9d19928c6
faroutnerd <farhan91179@gmail.com>
2025-04-22 20:46:06 +0530

    album completed

--- client/src/components/Display.jsx
+++ client/src/components/Display.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import DisplayHome from './DisplayHome'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import DisplayAlbum from './DisplayAlbum'
+import { albumsData } from '../assets/assets'
 
 const Display = () => {
+
+  const displayRef = useRef();  // using for album background color
+  const location = useLocation(); // to get the current location
+  const isAlbum = location.pathname.includes("album"); // check if the current location is album
+  const albumId = isAlbum ? location.pathname.slice(-1) : ""; // get the album id from the current location
+  const bgColor = albumsData[Number(albumId)].bgColor; // get the album color from the album id
+  
+  useEffect(()=>{
+    if(isAlbum){
+      displayRef.current.style.background = `linear-gradient(${bgColor}, #121212)`
+    } else {
+      displayRef.current.style.background = `#121212)`
+    }
+  });
+
   return (
-    <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
+    <div ref={displayRef} className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
       <Routes>
         <Route path="/" element={<DisplayHome />} />
+        <Route path="/album/:id" element={<DisplayAlbum />} />
       </Routes>
     </div>
   )
--- client/src/components/DisplayHome.jsx
+++ client/src/components/DisplayHome.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Navbar from './Navbar'
-import { albumsData } from '../assets/assets'
+import { albumsData, songsData } from '../assets/assets'
 import AlbumItem from './AlbumItem'
+import SongItem from './SongItem'
 
 const DisplayHome = () => {
   return (
@@ -10,9 +11,14 @@ const DisplayHome = () => {
         <div className="mb-4">
           <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
           <div className="flex overflow-auto">
-
+            {albumsData.map((item, index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
           </div>
-          {albumsData.map((item, index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
+        </div>
+        <div className="mb-4">
+          <h1 className='my-5 font-bold text-2xl'>Today's biggest hits</h1>
+          <div className="flex overflow-auto">
+            {songsData.map((item, index) => (<SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
+          </div>
         </div>
     </>
   )
